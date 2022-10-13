8492c45c6544bd5b09477820f426d68a58eba0a0
Slsachiya99 <115049370+Slsachiya99@users.noreply.github.com>
2022-10-13 19:03:26 +0530

    ~*♲❃BLACK-TIGER-MD-V6❃♲*~

---
+++ Setting.js
@@ -0,0 +1,23 @@
+{
+
+    "OWNER" : "SACHIYA-MD",
+
+    "NUMBER" : "+94767233346",
+
+    "YT_CHANNEL" : "https://www.youtube.com/channel/UCBi1evgI3DI3pwC_H5zK0kg",
+
+    "BLOCK_CHAT" : "120363043598019970@g.us",
+
+    "img" : "https://i.imgur.com/XHhlqtK.jpeg",
+
+    "support" : "https://chat.whatsapp.com/JLqwVWcONTkFRRfW89IcR5",
+
+    "logmsg" : "_SACHIYA-MD Bot Now Working 💌_\n\n*_👋 HI welcome to SACHIYA-MD bot Thanks To 💗🙌_*\n\n🔮 *_කරුණාකර මෙහි ප්ලගීන උත්සාහ නොකරන්න. මෙය ඔබගේ ලොග් අංකයයි. ⚙ ඔබට ඕනෑම චැට් එකක විධාන උත්සාහ කළ හැකිය*",
+
+    "SCRIPT" : "*👸🏻 Thanaks fro using SACHIYA MD Script ||",
+
+    "THUMB" : "https://i.imgur.com/XHhlqtK.jpeg",
+
+    "~*♲❃BLACK-TIGER-MD-V6❃♲*~"
+
+  }
